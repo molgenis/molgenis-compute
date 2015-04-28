44cc66256de288d2d9df4c30df342acd5d2b301b
Mark-de-Haan <markdehaan90@gmail.com>
2015-04-28 17:37:44 +0200

    Implemented AsyncStreamReader interface

--- molgenis-compute-core/src/main/java/org/molgenis/compute5/sysexecutor/AsyncStreamReader.java
+++ molgenis-compute-core/src/main/java/org/molgenis/compute5/sysexecutor/AsyncStreamReader.java
@@ -1,59 +1,24 @@
 package org.molgenis.compute5.sysexecutor;
 
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.log4j.Logger;
-
-class AsyncStreamReader extends Thread
+/**
+ * This class allows for the asynchronous reading of an input stream
+ */
+public interface AsyncStreamReader
 {
-	private static final Logger LOG = Logger.getLogger(AsyncStreamReader.class);
+	/**
+	 * Returns the buffer used to instantiate this {@link AsyncStreamReader}
+	 * 
+	 * @return A string representation of a {@link StringBuffer}
+	 */
+	public String getBuffer();
 
-	private StringBuffer buffer = null;
-	private InputStream inputStream = null;
-	private boolean stop = false;
-	private String newLine = null;
+	/**
+	 * 
+	 */
+	public void run();
 
-	public AsyncStreamReader(InputStream inputStream, StringBuffer buffer)
-	{
-		this.inputStream = inputStream;
-		this.buffer = buffer;
-		this.newLine = System.getProperty("line.separator");
-	}
-
-	public String getBuffer()
-	{
-		return buffer.toString();
-	}
-
-	public void run()
-	{
-		try
-		{
-			readCommandOutput();
-		}
-		catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void stopReading()
-	{
-		stop = true;
-	}
-
-	private void readCommandOutput() throws IOException
-	{
-		BufferedReader bufOut = new BufferedReader(new InputStreamReader(inputStream));
-		String line = null;
-		while ((stop == false) && ((line = bufOut.readLine()) != null))
-		{
-			buffer.append(line + newLine);
-			LOG.debug(line);
-		}
-		bufOut.close();
-	}
+	/**
+	 * 
+	 */
+	public void stopReading();
 }
---
+++ molgenis-compute-core/src/main/java/org/molgenis/compute5/sysexecutor/AsyncStreamReaderImpl.java
@@ -0,0 +1,67 @@
+package org.molgenis.compute5.sysexecutor;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+class AsyncStreamReaderImpl extends Thread implements AsyncStreamReader
+{
+	private static final Logger LOG = Logger.getLogger(AsyncStreamReaderImpl.class);
+
+	private StringBuffer buffer = null;
+	private InputStream inputStream = null;
+	private String newLine = System.getProperty("line.separator");
+	private boolean stop = false;
+
+	public AsyncStreamReaderImpl(InputStream inputStream, StringBuffer buffer)
+	{
+		this.inputStream = inputStream;
+		this.buffer = buffer;
+	}
+
+	@Override
+	public String getBuffer()
+	{
+		return buffer.toString();
+	}
+
+	@Override
+	public void run()
+	{
+		BufferedReader bufferedReader = null;
+		try
+		{
+			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null;
+			while ((stop == false) && ((line = bufferedReader.readLine()) != null))
+			{
+				buffer.append(line + newLine);
+				LOG.info(line);
+			}
+		}
+		catch (Exception e)
+		{
+			LOG.error("Error while reading the input stream: " + e);
+		}
+		finally
+		{
+			try
+			{
+				bufferedReader.close();
+			}
+			catch (IOException e)
+			{
+				LOG.error("Failed to close file: " + e);
+			}
+		}
+	}
+
+	@Override
+	public void stopReading()
+	{
+		stop = true;
+	}
+}
