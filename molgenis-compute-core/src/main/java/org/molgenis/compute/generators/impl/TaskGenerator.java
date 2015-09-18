5f9594a7938af3256ee72d8cc40926de92a2360a
RoanKanninga <roankanninga@gmail.com>
2015-09-18 09:55:13 +0200

    Changed permissions to 660

--- molgenis-compute-core/src/main/java/org/molgenis/compute/generators/impl/TaskGenerator.java
+++ molgenis-compute-core/src/main/java/org/molgenis/compute/generators/impl/TaskGenerator.java
@@ -512,7 +512,7 @@ public class TaskGenerator
 
 	private String appendToEnv(String script, String string, String thisFile)
 	{
-		String appendString = "echo \"" + string + "\" >> " + thisFile + "\n" + "chmod 760 " + thisFile + "\n";
+		String appendString = "echo \"" + string + "\" >> " + thisFile + "\n" + "chmod 660 " + thisFile + "\n";
 
 		return script + "\n" + appendString;
 	}
