d31adc47de15f5101fb005160f2612955328e0d9
Mark-de-Haan <markdehaan90@gmail.com>
2015-12-01 16:55:17 +0100

    Refactored the SeperatedListFoldingTest class to use the new style of script content testing

--- molgenis-compute-core/src/test/java/org/molgenis/compute/SeparatedListFoldingTest.java
+++ molgenis-compute-core/src/test/java/org/molgenis/compute/SeparatedListFoldingTest.java
@@ -1,97 +1,34 @@
 package org.molgenis.compute;
 
-import org.apache.commons.io.FileUtils;
-import org.molgenis.compute.ComputeCommandLine;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.File;
-
-/**
- * Created with IntelliJ IDEA. User: hvbyelas Date: 7/16/13 Time: 3:01 PM To change this template use File | Settings |
- * File Templates.
- */
-public class SeparatedListFoldingTest
+public class SeparatedListFoldingTest extends ScriptComparator
 {
-	private String outputDir = "target/test/benchmark/run";
-
 	@Test
 	public void testFoldOnTwoString() throws Exception
 	{
-		System.out.println("--- Start Test Folding 1---");
-
-		File f = new File(outputDir);
-		FileUtils.deleteDirectory(f);
-		Assert.assertFalse(f.exists());
-
-		f = new File(".compute.properties");
-		FileUtils.deleteQuietly(f);
-		Assert.assertFalse(f.exists());
+		System.out.println("--- Start Test Folding on two strings ---");
 
 		ComputeCommandLine.main(new String[]
 		{ "--generate", "--run", "--workflow", "src/main/resources/workflows/foreachtest/workflow.csv", "--parameters",
 				"src/main/resources/workflows/foreachtest/parameters.csv", "--parameters",
-				"src/main/resources/workflows/foreachtest/parameters1.csv", "--rundir", outputDir });
+				"src/main/resources/workflows/foreachtest/parameters1.csv", "--rundir", OUTPUT_DIRECTORY, "--runid",
+				"testFoldOnTwoString" });
 
-		System.out.println("--- Test Folding on Two Strings ---");
-
-		String test1_0_list1 = "chr[0]=\"1\"\n" + "chr[1]=\"2\"";
-		String test1_0_list2 = "sample[0]=\"sample1\"";
-		String test1_1_list1 = "chr[0]=\"1\"\n" + "chr[1]=\"2\"";
-		String test1_1_list2 = "sample[0]=\"sample2\"\n" + "sample[1]=\"sample3\"";
-		String test1_2_list1 = "chr[0]=\"1\"\n" + "chr[1]=\"2\"";
-		String test1_2_list2 = "sample[0]=\"sample4\"";
-
-		String t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_0.sh");
-
-		if (!t.contains(test1_0_list1) || !t.contains(test1_0_list2))
-		{
-			Assert.fail("folding broken");
-		}
-
-		t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_1.sh");
-
-		if (!t.contains(test1_1_list1) || !t.contains(test1_1_list2))
-		{
-			Assert.fail("folding broken");
-		}
-
-		t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_2.sh");
-
-		if (!t.contains(test1_2_list1) || !t.contains(test1_2_list2))
-		{
-			Assert.fail("folding broken");
-		}
-
+		testOutputDirectoryFiles("testFoldOnTwoString");
 	}
 
 	@Test
 	public void testParametersAll() throws Exception
 	{
-		System.out.println("--- Start Test Folding 1---");
-
-		File f = new File(outputDir);
-		FileUtils.deleteDirectory(f);
-		Assert.assertFalse(f.exists());
-
-		f = new File(".compute.properties");
-		FileUtils.deleteQuietly(f);
-		Assert.assertFalse(f.exists());
+		System.out.println("--- Start Test folding all parameters ---");
 
 		ComputeCommandLine.main(new String[]
 		{ "--generate", "--run", "--workflow", "src/main/resources/workflows/listOutOfTwo/workflow.csv", "--parameters",
-				"src/main/resources/workflows/listOutOfTwo/parametersall.csv", "--rundir", outputDir });
+				"src/main/resources/workflows/listOutOfTwo/parametersall.csv", "--rundir", OUTPUT_DIRECTORY, "--runid",
+				"testParametersAll" });
 
-		String proper = "combination[0]=\"prefix1postfix1\"\n" + "combination[1]=\"prefix2postfix2\"\n"
-				+ "combination[2]=\"prefix3postfix3\"";
-
-		String t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_0.sh");
-
-		if (!t.contains(proper))
-		{
-			Assert.fail("folding broken");
-		}
-
+		testOutputDirectoryFiles("testParametersAll");
 	}
 
 	/**
@@ -102,60 +39,26 @@ public class SeparatedListFoldingTest
 	@Test
 	public void testParameters2() throws Exception
 	{
-
-		System.out.println("--- Start Test Folding ---");
-
-		File f = new File(outputDir);
-		FileUtils.deleteDirectory(f);
-		Assert.assertFalse(f.exists());
-
-		f = new File(".compute.properties");
-		FileUtils.deleteQuietly(f);
-		Assert.assertFalse(f.exists());
+		System.out.println("--- Start Test Folding without all possible parameter combinations ---");
 
 		ComputeCommandLine.main(new String[]
 		{ "--generate", "--run", "--workflow", "src/main/resources/workflows/listOutOfTwo/workflow2.csv",
 				"--parameters", "src/main/resources/workflows/listOutOfTwo/parameters.csv", "--weave", "--rundir",
-				outputDir });
+				OUTPUT_DIRECTORY, "--runid", "testParameters2" });
 
-		String proper = "for s in \"prefix1\" \"prefix2\" \"prefix3\" \"postfix1\" \"postfix2\" \"postfix3\"";
-
-		String t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_0.sh");
-
-		if (!t.contains(proper))
-		{
-			Assert.fail("folding broken");
-		}
+		testOutputDirectoryFiles("testParameters2");
 	}
 
 	@Test
 	public void testParametersAllInTwoFiles() throws Exception
 	{
-		System.out.println("--- Start Test Folding 1---");
-
-		File f = new File(outputDir);
-		FileUtils.deleteDirectory(f);
-		Assert.assertFalse(f.exists());
-
-		f = new File(".compute.properties");
-		FileUtils.deleteQuietly(f);
-		Assert.assertFalse(f.exists());
+		System.out.println("--- Start Test Folding all parameters from two files ---");
 
 		ComputeCommandLine.main(new String[]
 		{ "--generate", "--run", "--workflow", "src/main/resources/workflows/listOutOfTwo/workflow.csv", "--parameters",
 				"src/main/resources/workflows/listOutOfTwo/parameters.csv", "--parameters",
-				"src/main/resources/workflows/listOutOfTwo/parameters1.csv", "--rundir", outputDir });
+				"src/main/resources/workflows/listOutOfTwo/parameters1.csv", "--rundir", OUTPUT_DIRECTORY, "--runid", "testParametersAllInTwoFiles"});
 
-		String proper = "combination[0]=\"prefix1postfix1\"\n" + "combination[1]=\"prefix2postfix2\"\n"
-				+ "combination[2]=\"prefix3postfix3\"";
-
-		String t = ComputeCommandLineTest.getFileAsString(outputDir + "/test1_0.sh");
-
-		if (!t.contains(proper))
-		{
-			Assert.fail("folding broken");
-		}
-
+		testOutputDirectoryFiles("testParametersAllInTwoFiles");
 	}
-
 }
---
+++ molgenis-compute-core/src/test/resources/expected/testFoldOnTwoString/submit.sh
@@ -0,0 +1,49 @@
+# First cd to the directory with the *.sh and *.finished scripts
+MOLGENIS_scriptsDir=$( cd -P "$( dirname "$0" )" && pwd )
+echo "cd $MOLGENIS_scriptsDir"
+cd $MOLGENIS_scriptsDir
+
+# Use this to indicate that we skip a step
+skip(){
+	echo "0: Skipped --- TASK '$1' --- ON $(date +"%Y-%m-%d %T")" >> molgenis.skipped.log
+}
+
+# Skip this step if step finished already successfully
+if [ -f test1_0.sh.finished ]; then
+	skip test1_0.sh
+	echo "Skipped test1_0.sh"
+else
+	echo "--- begin step: test1_0 ---"
+	echo " "
+	bash test1_0.sh
+	echo " "
+	echo "--- end step: test1_0 ---"
+	echo " "
+	echo " "
+fi
+# Skip this step if step finished already successfully
+if [ -f test1_1.sh.finished ]; then
+	skip test1_1.sh
+	echo "Skipped test1_1.sh"
+else
+	echo "--- begin step: test1_1 ---"
+	echo " "
+	bash test1_1.sh
+	echo " "
+	echo "--- end step: test1_1 ---"
+	echo " "
+	echo " "
+fi
+# Skip this step if step finished already successfully
+if [ -f test1_2.sh.finished ]; then
+	skip test1_2.sh
+	echo "Skipped test1_2.sh"
+else
+	echo "--- begin step: test1_2 ---"
+	echo " "
+	bash test1_2.sh
+	echo " "
+	echo "--- end step: test1_2 ---"
+	echo " "
+	echo " "
+fi
